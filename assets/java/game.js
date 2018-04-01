ea564f72edeefd76d707e2eed00204c1d7ca171e
Chris <chrisgoerler@gmail.com>
2018-04-01 18:13:04 -0400

    incremental design changes

--- assets/java/game.js
+++ assets/java/game.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
 $(".panel").click(function(){
     $("#fighter1").hide(1000);
     $("#header").text("Now Choose Your Oppenent!");
+    setTimeout(function(){ 
+        $(".panel-title").text("Opponent");
+    },1000);
+
+    
 });
 
 
