6c4df3a84ff21b3f0354128cf9178009c75e43d7
Chris <chrisgoerler@gmail.com>
2018-04-03 18:48:13 -0400

    ui update

--- assets/java/game.js
+++ assets/java/game.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
     var attack = 8; //set attack equal to 8
     var counterAttack = Math.floor(Math.random()*10)+14; //set the counter attack equal to a random number plus 14
     var isChosen = true;
-
+    var player;
+    var opponent;
     
     console.log(counterAttack);
 
 if(isChosen){
     $(".panel").click(function(){  
-        $(this).hide(1000);
+        $(this).hide(500);
+        player = $(this);
         
       
             setTimeout(function(){ 
@@ -16,8 +18,17 @@ if(isChosen){
                 $(".panel-title").text("Opponent");
             },1000);
     });
+    isChosen = false;
 }
+else{
+    $(".panel").click(function(){
+        $(this).hide(1000);
+        opponent = $(this);
 
+    });
+};
+console.log(opponent);
+console.log(player);
 
 // $(".panel").click(function(){
 //     $("#fighter2").hide(1000);
