5e4d34ac32b3287363586053cdcb5ab42d4b9536
Chris <chrisgoerler@gmail.com>
2018-04-03 16:25:07 -0400

    minor ui improvement and graphic changes

--- assets/java/game.js
+++ assets/java/game.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
-
-
-
-$(".panel").click(function(){
-    $("#fighter1").hide(1000);
-    $("#header").text("Now Choose Your Oppenent!");
-    setTimeout(function(){ 
-        $(".panel-title").text("Opponent");
-    },1000);
+    var attack = 8; //set attack equal to 8
+    var counterAttack = Math.floor(Math.random()*10)+14; //set the counter attack equal to a random number plus 14
+    var isChosen = true;
 
     
-});
+    console.log(counterAttack);
 
+if(isChosen){
+    $(".panel").click(function(){  
+        $(this).hide(1000);
+        
+      
+            setTimeout(function(){ 
+                $("#header").text("Now Choose Your Oppenent!");
+                $(".panel-title").text("Opponent");
+            },1000);
+    });
+}
+
+
+// $(".panel").click(function(){
+//     $("#fighter2").hide(1000);
+// })
 
 });
