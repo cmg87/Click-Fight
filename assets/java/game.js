2fc42d50024d7947ca2271348e91ed9c822e2f5a
Chris <chrisgoerler@gmail.com>
2018-04-04 16:31:22 -0400

    added in object and populated characters from jquery instead of direct placement in html.

--- assets/java/game.js
+++ assets/java/game.js
@@ -1,37 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var attack = 8; //set attack equal to 8
-    var counterAttack = Math.floor(Math.random()*10)+14; //set the counter attack equal to a random number plus 14
-    var isChosen = true;
+    var counterAttack = Math.floor(Math.random() * 10) + 14; //set the counter attack equal to a random number plus 14
+    var playerChosen = false;
+    var opponentChosen = false;
     var player;
     var opponent;
-    
-    console.log(counterAttack);
 
-if(isChosen){
-    $(".panel").click(function(){  
-        $(this).hide(500);
-        player = $(this);
+    var fighters = {
+        name: ["Scorpion", "Sub Zero", "Smoke", "Reptile"],
+        image: ["'assets/images/scorpion.png'", "'assets/images/subzero.png'", "'assets/images/smoke.png'", "'assets/images/reptile.png'"],
+        health: [200, 220, 190, 230],
+        sound: ['',"'assets/sounds/subzero.mp3'",'','']
+
+    };
+    var myobj = fighters.name.length;
+    for (i = 0; i < myobj; i++) {
+        var newcolumn = $("<div>");
+        newcolumn.addClass("col-xs-3 text-center selectable");
+        newcolumn.attr("data", fighters.health[i]);
+        newcolumn.text(fighters.name[i]);
+        newcolumn.append("<br><img src=" + fighters.image[i] + " />" + "<br>" + fighters.health[i] + ":HP");
+        $("#firstrow").append(newcolumn);
+    }
+
+    $(".selectable").click(function () {
+        if (playerChosen === false) {
+            $(this).animate({top: '250px'});
+            player = $(this);
+            playerChosen = true;
+            setTimeout(function () {
+                $("#header").text("Now Choose Your Oppenent!");    
+            }, 500);
         
-      
-            setTimeout(function(){ 
-                $("#header").text("Now Choose Your Oppenent!");
-                $(".panel-title").text("Opponent");
-            },1000);
-    });
-    isChosen = false;
-}
-else{
-    $(".panel").click(function(){
-        $(this).hide(1000);
-        opponent = $(this);
+
+
+        }
+        else if (playerChosen === true && opponentChosen === false) {
+            $(this).animate({top: '250px'});
+            opponent = $(this);
+            opponentChosen = true;
+            setTimeout(function () {
+                $("#header").text("Fight!!!"); 
+            }, 500);
+           
+        }
 
     });
-};
-console.log(opponent);
-console.log(player);
 
-// $(".panel").click(function(){
-//     $("#fighter2").hide(1000);
-// })
+
+
+
+
 
 });
